46bcf36a205403fa141e784b15a40dad52b31e77
Lencina97 <47856407+Lencina97@users.noreply.github.com>
2023-01-31 23:46:59 -0300

    Primer commit

---
+++ eventos.js
@@ -0,0 +1,62 @@
+
+document.getElementById("1").addEventListener(`click`,function(){
+    if (document.getElementById("educacion").style.display = "block") {
+        document.getElementById("educacion").style.display = "none"
+    }
+    if (document.getElementById("perfil").style.display = "none") {
+        document.getElementById("perfil").style.display = "block"
+    }
+    if (document.getElementById("experiencia").style.display = "block") {
+        document.getElementById("experiencia").style.display = "none"
+    }
+    if (document.getElementById("mas_sobre_mi").style.display = "block") {
+        document.getElementById("mas_sobre_mi").style.display = "none"
+    };
+})
+
+document.getElementById("2").addEventListener(`click`,function(){
+    if (document.getElementById("educacion").style.display = "none") {
+        document.getElementById("educacion").style.display = "block"
+    }
+    if (document.getElementById("perfil").style.display = "block") {
+        document.getElementById("perfil").style.display = "none"
+    }
+    if (document.getElementById("experiencia").style.display = "block") {
+        document.getElementById("experiencia").style.display = "none"
+    };  
+    if (document.getElementById("mas_sobre_mi").style.display = "block") {
+        document.getElementById("mas_sobre_mi").style.display = "none"
+    };
+})
+document.getElementById("3").addEventListener(`click`,function(){
+    if (document.getElementById("educacion").style.display = "block") {
+        document.getElementById("educacion").style.display = "none"
+    }
+    if (document.getElementById("perfil").style.display = "block") {
+        document.getElementById("perfil").style.display = "none"
+    }
+    if (document.getElementById("experiencia").style.display = "none") {
+        document.getElementById("experiencia").style.display = "block"
+    };
+    if (document.getElementById("mas_sobre_mi").style.display = "block") {
+        document.getElementById("mas_sobre_mi").style.display = "none"
+    };     
+})
+document.getElementById("4").addEventListener(`click`,function(){
+    if (document.getElementById("educacion").style.display = "block") {
+        document.getElementById("educacion").style.display = "none"
+    }
+    if (document.getElementById("perfil").style.display = "block") {
+        document.getElementById("perfil").style.display = "none"
+    }
+    if (document.getElementById("experiencia").style.display = "block") {
+        document.getElementById("experiencia").style.display = "none"
+    };
+    if (document.getElementById("mas_sobre_mi").style.display = "none") {
+        document.getElementById("mas_sobre_mi").style.display = "block"
+    };
+    
+})
+
+// Disculpen que repita tanto el código, pero es que tuve dificultades para hacer que funcione como quería y
+//ya no tenia tiempo para mejorarlo.
