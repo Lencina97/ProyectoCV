1b726a2528f1da8d2fa5a5d755d43f58a3c9c644
Lencina97 <47856407+Lencina97@users.noreply.github.com>
2023-02-28 21:32:54 -0300

    Add files via upload

--- eventos.js
+++ eventos.js
@@ -1,62 +1,43 @@
 
-document.getElementById("1").addEventListener(`click`,function(){
-    if (document.getElementById("educacion").style.display = "block") {
-        document.getElementById("educacion").style.display = "none"
-    }
-    if (document.getElementById("perfil").style.display = "none") {
-        document.getElementById("perfil").style.display = "block"
-    }
-    if (document.getElementById("experiencia").style.display = "block") {
-        document.getElementById("experiencia").style.display = "none"
-    }
-    if (document.getElementById("mas_sobre_mi").style.display = "block") {
-        document.getElementById("mas_sobre_mi").style.display = "none"
-    };
-})
+document.getElementById("1").addEventListener(`click`,cambiarPerfil)
 
-document.getElementById("2").addEventListener(`click`,function(){
-    if (document.getElementById("educacion").style.display = "none") {
-        document.getElementById("educacion").style.display = "block"
+document.getElementById("2").addEventListener(`click`,cambiarEducacion)
+
+document.getElementById("3").addEventListener(`click`,cambiarExperiencia)
+
+document.getElementById("4").addEventListener(`click`,cambiarDatos)
+
+function cambiarPerfil() {
+    cambiarTexto("perfil","educacion","mas_sobre_mi","experiencia")
+}
+function cambiarEducacion() {
+    cambiarTexto("educacion","experiencia","mas_sobre_mi","perfil")
+}
+function cambiarExperiencia(){
+    cambiarTexto("experiencia","educacion","mas_sobre_mi","perfil")
+}
+
+
+function cambiarDatos() {
+    cambiarTexto("mas_sobre_mi","educacion","perfil","experiencia")
+}
+
+function cambiarTexto(titulo1,titulo2,titulo3,titulo4) {
+    var titulo1 = document.getElementById(titulo1)
+    var titulo2 = document.getElementById(titulo2)
+    var titulo3 = document.getElementById(titulo3)
+    var titulo4 = document.getElementById(titulo4)
+    if (titulo1.style.display="none") {
+        titulo1.style.display="block"
     }
-    if (document.getElementById("perfil").style.display = "block") {
-        document.getElementById("perfil").style.display = "none"
+    if (titulo2.style.display="block") {
+        titulo2.style.display="none"
     }
-    if (document.getElementById("experiencia").style.display = "block") {
-        document.getElementById("experiencia").style.display = "none"
-    };  
-    if (document.getElementById("mas_sobre_mi").style.display = "block") {
-        document.getElementById("mas_sobre_mi").style.display = "none"
-    };
-})
-document.getElementById("3").addEventListener(`click`,function(){
-    if (document.getElementById("educacion").style.display = "block") {
-        document.getElementById("educacion").style.display = "none"
+    if (titulo3.style.display="block") {
+        titulo3.style.display="none"
     }
-    if (document.getElementById("perfil").style.display = "block") {
-        document.getElementById("perfil").style.display = "none"
+    if (titulo4.style.display="block") {
+        titulo4.style.display="none"
     }
-    if (document.getElementById("experiencia").style.display = "none") {
-        document.getElementById("experiencia").style.display = "block"
-    };
-    if (document.getElementById("mas_sobre_mi").style.display = "block") {
-        document.getElementById("mas_sobre_mi").style.display = "none"
-    };     
-})
-document.getElementById("4").addEventListener(`click`,function(){
-    if (document.getElementById("educacion").style.display = "block") {
-        document.getElementById("educacion").style.display = "none"
-    }
-    if (document.getElementById("perfil").style.display = "block") {
-        document.getElementById("perfil").style.display = "none"
-    }
-    if (document.getElementById("experiencia").style.display = "block") {
-        document.getElementById("experiencia").style.display = "none"
-    };
-    if (document.getElementById("mas_sobre_mi").style.display = "none") {
-        document.getElementById("mas_sobre_mi").style.display = "block"
-    };
     
-})
-
-// Disculpen que repita tanto el código, pero es que tuve dificultades para hacer que funcione como quería y
-//ya no tenia tiempo para mejorarlo.
+}
